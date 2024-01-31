7ade3a8f86ca8ef18d38562907915852151f28b9
PanthaganiAnanya <157079287+PanthaganiAnanya@users.noreply.github.com>
2024-01-30 21:54:52 -0700

    week-2

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+import StudentInfo from "./student-info";
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <p> Ananya Panthagani</p>
+      <StudentInfo>  </StudentInfo>
+    </main>
+ 
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+ 
+const StudentInfo = () => {
+  return (
+    <div>
+        <a href='https://github.com/Ananyapanthagani/panthaganiAnanya.github.io'> my github </a>
+      <h2>PanthaganiAnanya</h2>
+      <Link href="https://github.com">
+       my link github
+      </Link>
+    </div>
+  );
+};
+ 
+export default StudentInfo;
